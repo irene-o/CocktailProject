03173099986e66253aa1bbad0fe26f0f2e233a51
Irene Ogbevoen <x00192465@mytudublin.ie>
2024-12-11 23:04:35 +0000

    search complete

--- Cocktail.cs
+++ Cocktail.cs
@@ -1,5 +1,21 @@
-﻿namespace CocktailProject
+﻿using System.Text.Json.Serialization;
+
+namespace CocktailProject
 {
+    public class ApiResponse
+    {
+        public string Status { get; set; }
+        public object Error { get; set; }
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        public int Count { get; set; }
+        public string FilteredOn { get; set; }
+        public List<Cocktail> Cocktails { get; set; }
+    }
+
     public class Cocktail
     {
         public string Name { get; set; }
@@ -7,12 +23,14 @@
         public string Category { get; set; }
         public List<Ingredient> Ingredients { get; set; }
         public string Preparation { get; set; }
+        public string Garnish { get; set; }
     }
 
     public class Ingredient
     {
         public string Unit { get; set; }
-        public float? Amount { get; set; }
+        public double Amount { get; set; }
+        [JsonPropertyName("ingredient")]
         public string IngredientName { get; set; }
         public string Label { get; set; }
         public string Special { get; set; }
